e59335aef96d899a4c78e58e749e0fe50b685fd6
Thathsara Amarakoon <amarakoon.thathsara@gmail.com>
2025-09-04 10:29:55 +1200

    fix: rename ArrayAndLists.csproj to ArraysAndLists.csproj to match solution

--- part01_fundamentals/06_dates_and_times/src/Program.cs
+++ part01_fundamentals/06_dates_and_times/src/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 
 namespace DatesAndTimes;
 
@@ -6,6 +7,16 @@ public class Program
 {
     static void Main(string[] arg)
     {
-        Console.WriteLine("Hello World!");
+        var dateTime = new DateTime(2025, 01, 01);
+        var now = DateTime.Now;
+        var today = DateTime.Today;
+
+        Console.WriteLine("Hour: " + now.Hour);
+        Console.WriteLine("Minute: " + now.Minute);
+
+        var tomorrow = now.AddDays(1);
+        Console.WriteLine(tomorrow);
+
+        Console.WriteLine(now.ToString("yyyyy-MM-dd HH:mm"));
     }
 }
